57bb1d5b15bb3d786619fd8d4edbb69cef952c0a
David Reed <contact@dave-o-reed.co.uk>
2015-05-11 22:57:01 +0100

    Added puppet into the mix.

--- vpn.techpunch.com.tf
+++ vpn.techpunch.com.tf
@@ -28,20 +28,15 @@ resource "digitalocean_droplet" "vpn-techpunch-com" {
       "export PATH=$PATH:/usr/bin",
       "apt-get update",
       "apt-get -y upgrade",
-      "apt-get -y install openvpn",
       "mkdir -p /etc/openvpn/keys",
-      "cp /root/files/openvpn/config-store/vpn.techpunch.com.conf /etc/openvpn/",
       "cp /root/files/openvpn/key-store/ca.crt /etc/openvpn/keys/",
       "cp /root/files/openvpn/key-store/vpn.techpunch.com.crt /etc/openvpn/keys/",
       "cp /root/files/openvpn/key-store/vpn.techpunch.com.key /etc/openvpn/keys/",
       "cp /root/files/openvpn/key-store/dh2048.pem /etc/openvpn/keys/",
-      "cp -R /root/files/openvpn/client-configs/ /etc/openvpn/",
-      "sh /root/files/openvpn/firewall.sh",
-      "sh -c 'iptables-save > /etc/iptables.conf'",
-      "echo 'post-up iptables-restore < /etc/iptables.conf' >> /etc/network/interfaces",
-      "rm -rf /root/files",
-      "chmod -R 400 /etc/openvpn/keys",
-      "service openvpn@vpn.techpunch.com start"
+      "apt-get --force-yes -y install puppet",
+      "cp -R /root/files/puppet/* /etc/puppet/",
+      "puppet apply /etc/puppet/environments/production/manifests/site.pp --confdir=/etc/puppet/ --environment=production --environmentpath=/etc/puppet/environments/",
+      "rm -rf /root/files"
     ]
   }
 }
