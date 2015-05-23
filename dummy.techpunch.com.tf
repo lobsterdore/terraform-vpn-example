dd591933d201d5bb94724574f1265d5159c70dfe
David Reed <contact@dave-o-reed.co.uk>
2015-05-23 11:08:31 +0100

    Removed parts now controlled by puppet and added puppet provision to dummy box.

--- dummy.techpunch.com.tf
+++ dummy.techpunch.com.tf
@@ -29,21 +29,14 @@ resource "digitalocean_droplet" "dummy-techpunch-com" {
       "export PATH=$PATH:/usr/bin",
       "apt-get update",
       "apt-get -y upgrade",
-      "apt-get --force-yes -y install openvpn dnsmasq",
-      "cp /root/files/dns/dnsmasq.conf /etc/",
-      "service dnsmasq restart",
       "mkdir -p /etc/openvpn/keys",
-      "cp /root/files/openvpn/config-store/dummy.techpunch.com.conf /etc/openvpn/",
       "cp /root/files/openvpn/key-store/ca.crt /etc/openvpn/keys/",
       "cp /root/files/openvpn/key-store/dummy.techpunch.com.crt /etc/openvpn/keys/",
       "cp /root/files/openvpn/key-store/dummy.techpunch.com.key /etc/openvpn/keys/",
-      "cp -R /root/files/openvpn/client-configs/ /etc/openvpn/",
-      "sh /root/files/openvpn/firewall.sh",
-      "sh -c 'iptables-save > /etc/iptables.conf'",
-      "echo 'post-up iptables-restore < /etc/iptables.conf' >> /etc/network/interfaces",
-      "rm -rf /root/files",
-      "chmod -R 400 /etc/openvpn/keys",
-      "service openvpn@dummy.techpunch.com start"
+      "apt-get --force-yes -y install puppet",
+      "cp -R /root/files/puppet/* /etc/puppet/",
+      "puppet apply /etc/puppet/environments/production/manifests/site.pp --confdir=/etc/puppet/ --environment=production --environmentpath=/etc/puppet/environments/",
+      "rm -rf /root/files"
     ]
   }
 }
